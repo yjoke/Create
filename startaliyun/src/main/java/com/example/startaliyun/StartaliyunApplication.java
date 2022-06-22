8cf1c06cd6252f3cb255dcc298cde2f60730ab78
YunJia <sanchuanguli@163.com>
2022-06-22 09:37:46 +0800

    test commit4

--- startaliyun/src/main/java/com/example/startaliyun/StartaliyunApplication.java
+++ startaliyun/src/main/java/com/example/startaliyun/StartaliyunApplication.java
@@ -1,13 +1,17 @@
 package com.example.startaliyun;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.xml.bind.SchemaOutputResolver;
 
 @SpringBootApplication
 public class StartaliyunApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StartaliyunApplication.class, args);
+        System.out.println("hello");
     }
 
 }
