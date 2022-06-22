9862133560c7343c11db1921fda03bde2db80ce6
YunJia <sanchuanguli@163.com>
2022-06-22 09:34:59 +0800

    test commit

---
+++ startaliyun/src/test/java/com/example/startaliyun/StartaliyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startaliyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartaliyunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
