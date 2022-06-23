f592a1820d47ef2f470305815d68bc633af8858e
YunJia <sanchuanguli@163.com>
2022-06-23 10:09:32 +0800

    delete import

--- startaliyun/src/main/java/com/example/startaliyun/StartaliyunApplication.java
+++ startaliyun/src/main/java/com/example/startaliyun/StartaliyunApplication.java
@@ -1,10 +1,8 @@
 package com.example.startaliyun;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.xml.bind.SchemaOutputResolver;
 
 @SpringBootApplication
 public class StartaliyunApplication {
