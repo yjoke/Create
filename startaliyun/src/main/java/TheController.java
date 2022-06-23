0b332b4d7833c921ccbf3763a52432f9493ffef6
YunJia <sanchuanguli@163.com>
2022-06-23 22:15:06 +0800

    commit hello

--- startaliyun/src/main/java/TheController.java
+++ startaliyun/src/main/java/TheController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public class TheController {
 
     @RequestMapping("/test")
--- startaliyun/src/test/java/com/example/startaliyun/StartaliyunApplicationTests.java
+++ startaliyun/src/test/java/com/example/startaliyun/StartaliyunApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.startaliyun;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StartaliyunApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
