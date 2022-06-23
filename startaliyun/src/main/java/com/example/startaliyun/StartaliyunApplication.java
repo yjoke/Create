0757fbcac1a2533315adbd6cd0bb153949b4c6e7
YunJia <sanchuanguli@163.com>
2022-06-23 08:43:39 +0800

    push test

--- startaliyun/src/main/java/com/example/startaliyun/StartaliyunApplication.java
+++ startaliyun/src/main/java/com/example/startaliyun/StartaliyunApplication.java
@@ -16,6 +16,7 @@ public class StartaliyunApplication {
         System.out.println("hello");
         System.out.println("master 冲突测试");
         System.out.println("冲突提交");
+        System.out.println("push test");
     }
 
 }
