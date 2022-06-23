74123825f36e947ddd7bc0ed052e4f99ed0b5dd0
YunJia <sanchuanguli@163.com>
2022-06-23 17:38:18 +0800

    controllerTest

---
+++ startaliyun/src/main/java/TheController.java
@@ -0,0 +1,17 @@
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HeYunjia
+ */
+
+@RestController
+@CrossOrigin("*")
+public class TheController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "spring boot test project";
+    }
+}
