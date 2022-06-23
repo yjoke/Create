877adb99d52bb9e05c2dabde661425fd9c61bc83
YunJia <sanchuanguli@163.com>
2022-06-23 22:46:19 +0800

    commit over

--- startaliyun/src/main/java/com/example/startaliyun/TheController.java
+++ startaliyun/src/main/java/com/example/startaliyun/TheController.java
@@ -1,3 +1,5 @@
+package com.example.startaliyun;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
